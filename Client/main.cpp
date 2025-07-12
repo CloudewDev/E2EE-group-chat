e3ff8743de24c6010bfc2bd4de92b6057367cc57
cloudew <cloudewdev@gmail.com>
2025-07-12 20:03:51 +0900

    mutex problem solved

--- Client/main.cpp
+++ Client/main.cpp
@@ -63,15 +63,12 @@ public:
 	
 	void watchKeyboard(){
 		while(!thread_controller.program_done.load()){
-			std::lock_guard<std::mutex> lock(thread_controller.cout_mutex);
-			std::cout << "me : ";
 			getline(std::cin, message_to_send);
 			
 			if (thread_controller.program_done.load()) break;
 			
 			if (message_to_send == "/quit"){
 				thread_controller.program_done.store(true);
-				
 				shutdown(server_connector.getSock(), SD_BOTH);
             	closesocket(server_connector.getSock());
 				break;
@@ -79,6 +76,9 @@ public:
 			else{
 				if (send(server_connector.getSock(), message_to_send.c_str(), message_to_send.length(), 0) < 0){
 					throw std::system_error(WSAGetLastError(), std::system_category(), "send failed");
+					std::lock_guard<std::mutex> lock(thread_controller.cout_mutex);
+					std::cout << "\r" << std::string(80, ' ') << "\r"; // erase current line
+					std::cout << "me : " << message_to_send << std::endl;
 				}
 			}
 		}
